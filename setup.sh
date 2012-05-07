ce81a9af16c3a4daa5797352af132f1960f32de1
Orion Buske <buske@cs.toronto.edu>
2012-05-07 17:56:25 -0400

    Finished draft of setup script

--- setup.sh
+++ setup.sh
@@ -3,18 +3,50 @@
 set -eu
 set -o pipefail
 
+python_version=$(python -c "import sys; print sys.version[:3]")
+
+function prompt {
+    read -p "(Press ENTER to continue, Ctrl-C to exit) "
+}
+
 # Download unafold
-wget http://mfold.rna.albany.edu/cgi-bin/UNAFold-download.cgi?unafold-3.8.tar.gz
-tar -xzf unafold-3.8.tar.gz
-cd unafold-3.8
-./configure && make
+if [[ ! -e tools/unafold/src/hybrid-ss-min ]]; then
+    echo "Downloading UNAFold 3.8..." >&2
+    prompt
+    pushd tools
+    if [[ ! -e unafold-3.8.tar.gz ]]; then
+	wget http://mfold.rna.albany.edu/cgi-bin/UNAFold-download.cgi?unafold-3.8.tar.gz
+    fi
+    if [[ ! -d unafold ]]; then
+	tar -xzf unafold-3.8.tar.gz
+	mv unafold-3.8 unafold
+    fi
+    pushd unafold
+    ./configure && make
+    popd
+    popd
+fi
+
 
 # Download data
-
+if [[ ! -e data/refGene.pkl.gz ]]; then
+    echo "Downloading synorder $version databases..." >&2
+    prompt
+    if [[ ! -e synorder_${version}_data.tar.gz ]]; then
+	wget http://compbio.cs.toronto.edu/synorder/synorder_${version}_data.tar.gz
+    fi
+    if [[ ! -d data ]]; then
+	tar -czf synorder_${version}_data.tar.gz
+    fi
+fi
 
 # Install modified version of milk
-prefix="$(pwd)"
-cd tools/milk
-export PYTHONPATH="$prefix:$PYTHONPATH"
-python setup.py install --prefix="$prefix"
-
+prefix="$(pwd)/lib/python${python_version}"
+if [[ ! -d $prefix/milk-*.egg ]]; then
+    echo "Configuring modified milk Python package..." >&2
+    prompt
+    pushd tools/milk
+    export PYTHONPATH="$prefix:$PYTHONPATH"
+    python setup.py install --prefix="$prefix"
+    popd
+fi
